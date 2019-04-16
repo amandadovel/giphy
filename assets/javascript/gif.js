6dde652bb7c54391c27302843e57201ef33088f2
Amanda Dovel <amandacdovel@gmail.com>
2019-04-16 16:24:24 -0600

    trying to figure out how to animate

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -55,30 +55,75 @@ $("button").on("click", function() {
     console.log(selectedGif);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     selectedGif + "&api_key=7EiJY9mrc7EqEPuPawrHUGKTDq4AG5pX&limit=10";
-
+   
     $.ajax({
         url: queryURL,
         method: "GET"
     })
 
     .then(function(response){
-    console.log(response);
+        var results = response.data;
+        console.log(results);
+    
 
     // We want to get the title, rating, and 2 images, still and animated
     // For each response item we want to create a div container, create an image, and some text elements
 
-    // for(var i = 0; i < results.length; i ++) {
-    //     var div = $("<div>").addClass("gifwrap");
-    //     var image = $("<img>").attr("src", results[i].images.original_still.url).attr("alt", results[i].title).data("still", results[i].images.original_still.url).data("animate", results[i].images.original.url);
-    //     var h3 = $("<h3>").text(results[i].rating);
-    //     var p = $("<p>").text(results[i].title);
-    //     $(div).append(image, h3, p);
-    //     $("gif-view").append(div);
-    // }
+     for(var i = 0; i < results.length; i ++) {
+
+        //div container 
+         var div = $("<div>").addClass("gifwrap");
+
+         // image tag to display results
+         var image = $("<img>");
+         image.addClass("result");
+
+         // src displays still gif when first loaded
+         image.attr("src", results[i].images.original_still.url);
+
+         // add alt attribute
+         image.attr("alt", results[i].title);
+
+         // add data attributes to make gifs animate 
+         image.data("data-state", "still");
+         image.data("data-still", results[i].images.original_still.url);
+         image.data("data-animate", "animate", results[i].images.original.url);
+
+         // variables to hold rating and title
+         var h3 = $("<h3>").text(results[i].rating);
+         var p = $("<p>").text(results[i].title);
+
+         $(div).append(image, h3, p);
+         $("#gif-view").prepend(div);
+     }
     })
+
 })
 
-//renderButtons();
+    // .on click function to play and pause gif
+
+    $("result").on("click", function(){
+        
+        // create variable to hold data-state of clicked gif
+        
+            var state = $(this).attr("data-state");
+            console.log(state);
+        
+        // if data state is still then click will change to animate 
+            if(state.attr("data-state") === "still") {
+                state.attr("src", state.attr("data-animate"));
+                state.attr("data-state", "animate");
+
+        // if data state is animated click will change to still 
+            }else{
+                state.attr("src", state.attr("data-still"));
+                state.attr("data-state", "still");
+            }
+        });
+
+
+
+
 
 
 
