0d788d2c9e9cfb03f99675ef4906388385872fd3
Amanda Dovel <amandacdovel@gmail.com>
2019-04-16 19:39:37 -0600

    finished javascript

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -24,102 +24,109 @@ renderButtons();
 
 // Function that handles events where one button is clicked 
 
-    $("#add-gif").on("click", function(event) {
-        event.preventDefault();
+$("#add-gif").on("click", function (event) {
+    event.preventDefault();
 
 
-// Get the value from the input, save it to a variable
-// Normalize the data, check if the value is inside of the movies array
-// if its not we want to add it and call on renderButtons function again
+    // Get the value from the input, save it to a variable
+    // Normalize the data, check if the value is inside of the movies array
+    // if its not we want to add it and call on renderButtons function again
 
     var newGif = $("#gif-input").val().trim().toLowerCase();
 
-    if(gifs.indexOf(newGif) === -1 && newGif !== "") {
+    if (gifs.indexOf(newGif) === -1 && newGif !== "") {
         gifs.push(newGif);
         $("#gif-input").val("");
+        $("#buttons-view").append(newGif);
         renderButtons();
 
-    }else{
-    $("#alert-message").text("Try another Giphy!");
-    $("#gif-input").val("");
-  }
+    } else {
+        $("#alert-message").text("Try another Giphy!");
+        $("#gif-input").val("");
+    }
 
 });
 
-$("#gif-input").on("click", function() {
+
+
+$("#gif-input").on("click", function () {
     $("#alert-message").text("");
 })
 
-$("button").on("click", function() {
+$("#buttons-view").on("click", ".gif-btn", function () {
     var selectedGif = $(this).data("gif");
-    console.log(selectedGif);
+    // console.log(selectedGif);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    selectedGif + "&api_key=7EiJY9mrc7EqEPuPawrHUGKTDq4AG5pX&limit=10";
-   
+        selectedGif + "&api_key=7EiJY9mrc7EqEPuPawrHUGKTDq4AG5pX&limit=10";
+
     $.ajax({
         url: queryURL,
         method: "GET"
     })
 
-    .then(function(response){
-        var results = response.data;
-        console.log(results);
-    
+        .then(function (response) {
+            var results = response.data;
+            console.log(results);
 
-    // We want to get the title, rating, and 2 images, still and animated
-    // For each response item we want to create a div container, create an image, and some text elements
 
-     for(var i = 0; i < results.length; i ++) {
+            // We want to get the title, rating, and 2 images, still and animated
+            // For each response item we want to create a div container, create an image, and some text elements
 
-        //div container 
-         var div = $("<div>").addClass("gifwrap");
+            for (var i = 0; i < results.length; i++) {
 
-         // image tag to display results
-         var image = $("<img>");
-         image.addClass("result");
+                //div container 
+                var div = $("<div>").addClass("gifwrap");
 
-         // src displays still gif when first loaded
-         image.attr("src", results[i].images.original_still.url);
+                // image tag to display results
+                var image = $("<img>");
+                image.addClass("result");
 
-         // add alt attribute
-         image.attr("alt", results[i].title);
+                // src displays still gif when first loaded
+                image.attr("src", results[i].images.fixed_height_still.url);
 
-         // add data attributes to make gifs animate 
-         image.data("data-state", "still");
-         image.data("data-still", results[i].images.original_still.url);
-         image.data("data-animate", "animate", results[i].images.original.url);
+                // add alt attribute
+                image.attr("alt", results[i].title);
 
-         // variables to hold rating and title
-         var h3 = $("<h3>").text(results[i].rating);
-         var p = $("<p>").text(results[i].title);
+                // add data attributes to make gifs animate 
+                image.attr("data-state", "still");
+                image.attr("data-still", results[i].images.fixed_height_still.url);
+                image.attr("data-animate", results[i].images.fixed_height.url);
 
-         $(div).append(image, h3, p);
-         $("#gif-view").prepend(div);
-     }
-    })
+                // variables to hold rating and title
+                var h3 = $("<h3>").text(results[i].rating);
+                var p = $("<p>").text(results[i].title);
+
+                $(div).append(image, h3, p);
+                $("#gif-view").prepend(div);
+            }
+        })
 
 })
 
-    // .on click function to play and pause gif
 
-    $("result").on("click", function(){
-        
-        // create variable to hold data-state of clicked gif
-        
-            var state = $(this).attr("data-state");
-            console.log(state);
-        
-        // if data state is still then click will change to animate 
-            if(state.attr("data-state") === "still") {
-                state.attr("src", state.attr("data-animate"));
-                state.attr("data-state", "animate");
+// .on click function to play and pause gif
+
+$(document).on("click", ".result", function () {
+
+    // create variable to hold data-state of clicked gif
+
+    var state = $(this).attr("data-state");
+    // console.log(state);
+
+    // if data state is still then click will change to animate 
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
 
         // if data state is animated click will change to still 
-            }else{
-                state.attr("src", state.attr("data-still"));
-                state.attr("data-state", "still");
-            }
-        });
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
+
+
+
 
 
 
